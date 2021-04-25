67896f7aaa40bd6a6e351804ca00fd7bf03408dd
honorezemagho <honorezemagho@gmail.com>
2021-04-25 20:39:11 +0100

    java strings

--- Main.java
+++ Main.java
@@ -1,28 +1,12 @@
-import java.util.Date;
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now.getTime());
-
-        // Copying variables
-        // 1.Primitives Types
-        byte month = 12;
-        byte myMonth = month;
-        myMonth = 8;
-        System.out.println(month);
-
-        // Reference Types
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point2.x = 10;
-        System.out.println(point1);
+        String message = "Hello World" + "!!";
+        System.out.println(message);
+        System.out.println(message.endsWith("!!"));
+        System.out.println("The length is " + message.length());
     }
 }
 
 /**
- * While assigning values in Primitives types the value is passed but in
- * Reference Types the memory reference is passed.
+ * String like others have multiples members/methods. Strings are Immutables
  */
