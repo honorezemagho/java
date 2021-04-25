b861df6334dd38a820ca672ed126fcdc8e07e129
honorezemagho <honorezemagho@gmail.com>
2021-04-25 18:54:45 +0100

    primitve types

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        byte age = 30;
+        int viewsCount = 123_456_789;
+        long population = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println(age);
     }
 }
+
+/**
+ * We have two categories of types in Java Primitive types(for storing simple
+ * values) - Reference or non-primitive type (for storing complex objects)
+ */
