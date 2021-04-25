e9632f09fa685eb60bd7627d7e54461745eb0c5d
honorezemagho <honorezemagho@gmail.com>
2021-04-25 20:53:25 +0100

    Arrays basics

--- Main.java
+++ Main.java
@@ -1,12 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello World" + "!!";
-        System.out.println(message);
-        System.out.println(message.endsWith("!!"));
-        System.out.println("The length is " + message.length());
+        byte number = 1;
+        int[] numbers = new int[5];
+
+        // if we have the arrays values
+        int[] actualNumbers = { 2, 3, 5, 1, 4, 6, 8, 10, 7, 9 };
+
+        numbers[0] = 1;
+        numbers[1] = 2;
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(actualNumbers);
+        Arrays.sort(actualNumbers);
+        System.out.println(Arrays.toString(actualNumbers));
+
     }
 }
 
 /**
- * String like others have multiples members/methods. Strings are Immutables
+ * Java Arrays have a fixed sizes
  */
