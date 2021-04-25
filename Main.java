98927278e279e07dc69f2396fc0f45588e9dd219
honorezemagho <honorezemagho@gmail.com>
2021-04-25 21:19:25 +0100

    arithmetics operations

--- Main.java
+++ Main.java
@@ -2,10 +2,18 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        final float pi = 3.14F;
+        byte addition = 1 + 8;
+        byte substraction = 8 - 3;
+        int multiplication = 45 * 89;
+        byte modulo = 74 % 5;
+        // Casting in java
+        double division = (double) 17 / (double) 3;
+
+        System.out.println("Addition " + addition + " Substraction " + substraction + " Multiplication "
+                + multiplication + " Division " + division + " Modulo " + modulo);
     }
 }
 
 /**
- * For constant prefix them with "final"
+ * 
  */
