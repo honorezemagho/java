646779a8d260c1ae6d7f97c8f1ef10021c33046e
honorezemagho <honorezemagho@gmail.com>
2021-04-25 20:23:48 +0100

    copying values

--- Main.java
+++ Main.java
@@ -1,16 +1,28 @@
+import java.util.Date;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         byte age = 30;
-        int viewsCount = 123_456_789;
-        long population = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-        System.out.println(age);
+        Date now = new Date();
+        System.out.println(now.getTime());
+
+        // Copying variables
+        // 1.Primitives Types
+        byte month = 12;
+        byte myMonth = month;
+        myMonth = 8;
+        System.out.println(month);
+
+        // Reference Types
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point2.x = 10;
+        System.out.println(point1);
     }
 }
 
 /**
- * We have two categories of types in Java Primitive types(for storing simple
- * values) - Reference or non-primitive type (for storing complex objects)
+ * While assigning values in Primitives types the value is passed but in
+ * Reference Types the memory reference is passed.
  */
