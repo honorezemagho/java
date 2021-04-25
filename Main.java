a4605f59932c66528f26ca647dc0b4747c7121e9
honorezemagho <honorezemagho@gmail.com>
2021-04-25 21:01:03 +0100

    java arrays

--- Main.java
+++ Main.java
@@ -2,20 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        byte number = 1;
-        int[] numbers = new int[5];
+        int[][] numbers = new int[2][3];
+        numbers[0][1] = 2;
 
-        // if we have the arrays values
-        int[] actualNumbers = { 2, 3, 5, 1, 4, 6, 8, 10, 7, 9 };
-
-        numbers[0] = 1;
-        numbers[1] = 2;
-
-        System.out.println(Arrays.toString(numbers));
-        System.out.println(actualNumbers);
-        Arrays.sort(actualNumbers);
-        System.out.println(Arrays.toString(actualNumbers));
-
+        int[][] actualNumbers = { { 1, 2, 3 }, { 4, 5, 6 } };
+        System.out.println(Arrays.deepToString(numbers));
+        System.out.println(Arrays.deepToString(actualNumbers));
     }
 }
 
