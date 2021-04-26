c7d31dd280e5966f926c63b432dd4a8da50be39f
honorezemagho <honorezemagho@gmail.com>
2021-04-26 13:23:43 +0100

    generate ower and uppercases using java

--- learning/basics/LowerUpperCases.java
+++ learning/basics/LowerUpperCases.java
@@ -1,14 +1,16 @@
-public class LowerAscii {
-    public function upperAscii(){
+public class LowerUpperCases {
+
+    public static void upperAscii(){
         StringBuilder sb = new StringBuilder(26);
         for (char ch = 'A'; ch <= 'Z'; ch++)
             sb.append(ch);
-        System.out.printf("lower ascii: %s, length: %s", sb, sb.length());
+        System.out.printf("upper ascii: %s, length: %s", sb, sb.length());
     }
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder(26);
         for (char ch = 'a'; ch <= 'z'; ch++)
             sb.append(ch);
+            upperAscii();
         System.out.printf("lower ascii: %s, length: %s", sb, sb.length());
     }
 }
