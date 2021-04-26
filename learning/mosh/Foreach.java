b7847ca7fb4f79f07e67dbd0b4f7848966bbdd7d
honorezemagho <honorezemagho@gmail.com>
2021-04-26 16:23:29 +0100

    Foreach in java

---
+++ learning/mosh/Foreach.java
@@ -0,0 +1,9 @@
+public class Foreach {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple","Mango","Orange"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
