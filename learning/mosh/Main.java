1fca3a0ef4d015d7202471ff695ce8ebe1cb1c82
honorezemagho <honorezemagho@gmail.com>
2021-04-26 17:51:04 +0100

    Mortgage calculator with validation

--- learning/mosh/Main.java
+++ learning/mosh/Main.java
@@ -7,15 +7,39 @@ public class Main {
         final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal Amount: ");
-        int principal = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
+        int principal = 0;
+        float annualInterest = 0;
+
+        while(true) {
+            System.out.print("Principal Amount ($100 - $1M): ");
+            principal = scanner.nextInt();
+            if(principal >= 100 && principal <= 1_000_000)
+                break;
+            System.out.print("Enter a value between 100 and 1_000_000");
+        }
+
+        
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest  = scanner.nextFloat();
+            if(annualInterest >= 1 && annualInterest <= 30)
+                break;
+            System.out.print("Enter a value between 1 and 30: ");
+        }
+        
+        
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
 
-        System.out.print("Period in (Years): ");
-        byte years = scanner.nextByte();
+        byte years = 0;
+        while(true) {
+            System.out.print("Period in (Years): ");
+            years = scanner.nextByte();  
+            if(years >= 1 && years <= 30)
+                break;
+                System.out.print("Enter a value between 1 and 30: ");   
+        }
+
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal 
