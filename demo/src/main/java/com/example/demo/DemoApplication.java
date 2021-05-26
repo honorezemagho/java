df5487b4ffe4bcdcbbd5fe4df90ed522d4b8ed6d
honorezemagho <honorezemagho@gmail.com>
2021-05-26 10:58:46 +0100

    migrate requests into a controller and add a service

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,41 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@GetMapping("/")
-	public String welcome() {
-		return "<h1>Welcome to our website</h1>";
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
-
-	@GetMapping("/student")
-	public List<Student> student() {
-		return List.of(new Student(
-				1L,"Free","free@free.fr",14,
-				LocalDate.of(2015, Month.JANUARY,5)
-		));
-	}
-
-
 }
