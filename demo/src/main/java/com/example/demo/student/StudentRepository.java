bd12c3a2041187353a0dfae479a6686ee1238a70
honorezemagho <honorezemagho@gmail.com>
2021-05-26 15:58:02 +0100

    implement add student and check email duplicates

--- demo/src/main/java/com/example/demo/student/StudentRepository.java
+++ demo/src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,14 +1,14 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+import java.util.Optional;
 
 // This interface is responsible for data access
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
-
+//    @Query("SELECT s FROM Student WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -3,12 +3,13 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Service
-public class StudentService {
+public class StudentService{
 
     private final StudentRepository studentRepository;
 
@@ -17,7 +18,36 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
+    public static boolean isValidEmail(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
     public List<Student> getStudents(){
                 return this.studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> checkEmail = studentRepository
+                .findStudentByEmail(student.getEmail());
+
+        boolean validEmail = isValidEmail(student.getEmail());
+        if(checkEmail.isPresent()) {
+            throw new IllegalStateException("Email is already taken. Please choose another one");
+        }
+
+        if (!validEmail){
+            throw new IllegalStateException("Invalid Email");
+        }
+
+        this.studentRepository.save(student);
     }
 }
